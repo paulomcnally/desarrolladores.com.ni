7bb743bc06482fd3f9866242dbdc1a7d08ee82ef
paulomcnally <paulomcnally@gmail.com>
2013-08-06 22:26:44 -0600

    0.0.3

--- controllers/system.js
+++ controllers/system.js
@@ -1,10 +1,14 @@
 // load require modules
+var sys = require('sys')
+var exec = require('child_process').exec;
 var fs = require('fs');
 var md = require("node-markdown").Markdown;
 var path = require('path');
 var crypto = require('crypto');
 var moment = require('moment');
 moment.lang("es");
+
+function puts(error, stdout, stderr) { sys.puts(stdout) }
 
 // create public function article
 exports.article = function (req, res) {
@@ -72,20 +76,30 @@ exports.article = function (req, res) {
 
 exports.update = function (req, res) {
     function out() {
+        // download new changes from git
+        exec("git pull", puts);
+
+
+        // set directory files
         var dir = path.join(__dirname, '../articles/', '');
+
 
         var data = new Array();
 
+        // read directory
         fs.readdir(dir, function (err, files) {
             if (err) throw err;
             var c = 0;
 
+            // recore files
             files.forEach(function (file) {
 
-
+                // read file
                 fs.readFile(dir + file, 'utf-8', function (err, html) {
                     c++;
                     if (err) throw err;
+
+                    // if file mime/type = text/json
                     if (file.split('.').pop() === "json") {
                         var json = JSON.parse(html);
                         var obj_item = {};
@@ -96,26 +110,27 @@ exports.update = function (req, res) {
                         data.push(obj_item);
                     }
 
+                    // if finish load al files
                     if (files.length == c) {
+                        // order object by date
                         var new_item = data.sort(function (a, b) {
                             return (new Date(b.date_format).getTime() - new Date(a.date_format).getTime());
                         });
 
+
+                        // create a json objet
                         var export_json = {};
                         export_json.title = 'Desarrolladores Nicaragua';
                         export_json.type = 'home';
                         export_json.articles = new_item;
-                        // load article template with article object
 
+                        // export a json file
                         fs.writeFile("./articles.json", JSON.stringify(export_json), function(error) {
                             if( error ){
                                 res.send(error)
                             }
                             res.redirect('/');
                         });
-
-
-
                     }
 
                 });
