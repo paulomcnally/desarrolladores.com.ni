a0335c4a1948bc9e2bc4bcffa2fa7a53ff0bd9a4
paulomcnally <paulomcnally@gmail.com>
2013-08-08 00:27:44 -0600

    0.0.7

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', controllers.system.home);
+app.get('/articulos', controllers.system.articles);
 app.get('/articulo/:slug', controllers.system.article);
-app.get('/hangouts/:slug', controllers.system.hangouts);
+app.get('/hangouts', controllers.system.hangouts);
+app.get('/hangout/:slug', controllers.system.hangout);
+app.get('/noticias', controllers.system.news);
+app.get('/noticia/:slug', controllers.system.new);
 app.post('/hook', controllers.system.hook);
 
 app.listen(1934);
--- controllers/system.js
+++ controllers/system.js
@@ -9,67 +9,47 @@ var moment = require('moment');
 var config = require('../config');
 moment.lang("es");
 
+// command lines puts
 function puts(error, stdout, stderr) {
     sys.puts(stdout);
 }
 
-// create public function article
-exports.article = function (req, res) {
+// home
+exports.home = function (req, res) {
     function out() {
+        var data = {};
+        data.title = config.web.title;
+        data.type = 'home';
+        var path_articles = path.join(__dirname, '../', 'articles.json');
+        var path_hangouts = path.join(__dirname, '../', 'hangouts.json');
+        var path_news = path.join(__dirname, '../', 'news.json');
 
-        // set path to article files
-        var path_article_json = path.join(__dirname, '../' + config.content.repository.name + '/articles', req.params.slug + '.json');
-        var path_article_markdown = path.join(__dirname, '../' + config.content.repository.name + '/articles', req.params.slug + '.markdown');
-
-        // load article json
-        fs.readFile(path_article_json, 'utf8', function (err1, article_json) {
-            if (err1) {
-                res.send('Error loading JSON file');
+        fs.readFile(path_articles, 'utf-8', function (err, articles) {
+            if (err){
+                res.send('Error loading JSON articles');
             }
-            else {
-                // load article markdown
-                fs.readFile(path_article_markdown, 'utf8', function (err2, article_markdown) {
-                    if (err2) {
-                        res.send('Error loading MARKDOWN file');
+            else{
+                // set articles object
+                data.articles = JSON.parse( articles );
+                fs.readFile(path_hangouts, 'utf-8', function (err, hangouts) {
+                    if (err){
+                        res.send('Error loading JSON hangouts');
                     }
-                    else {
-                        // set article object
-                        var article_obj = JSON.parse(article_json);
-
-                        // load author json
-                        var path_author_json = path.join(__dirname, '../' + config.content.repository.name + '/authors', article_obj.author + '.json');
-
-                        fs.readFile(path_author_json, 'utf8', function (err3, author_json) {
-                            if (err3) {
-                                res.send('Error loading MARKDOWN file');
+                    else{
+                        // set hangouts object
+                        data.hangouts = JSON.parse( hangouts );
+                        fs.readFile(path_news, 'utf-8', function (err, news) {
+                            if (err){
+                                res.send('Error loading JSON news');
                             }
-                            else {
-                                // set markdown to html content in article object
-                                article_obj.content = md(article_markdown);
-
-                                // set data from string reading
-                                article_obj.date = moment(article_obj.date).fromNow();
-
-                                if (article_obj.update != "") {
-                                    article_obj.update = moment(article_obj.update).fromNow();
-                                }
-
-                                // set author object
-                                var author_obj = JSON.parse(author_json);
-
-                                // set md5 hash to load picture from gravatar.com
-                                author_obj.picture = crypto.createHash('md5').update(author_obj.email).digest("hex");
-
-                                // set author object on article object
-                                article_obj.author = author_obj;
-
-                                // load article template with article object
-                                res.render('article', { data: article_obj });
+                            else{
+                                data.news = JSON.parse( news );
+                                res.render('index', { data: data });
                             }
                         });
-
                     }
                 });
+
             }
         });
     }
@@ -77,12 +57,11 @@ exports.article = function (req, res) {
     return out();
 }
 
+// hook
 exports.hook  = function (req, res) {
-    function out() {
-        exec(config.repository.exec, puts);
-
+    function create( type ){
         // set directory files
-        var dir = path.join(__dirname, '../' + config.content.repository.name + '/articles/', '');
+        var dir = path.join(__dirname, '../' + config.content.repository.name + '/' +type + '/', '');
 
 
         var data = new Array();
@@ -113,6 +92,10 @@ exports.hook  = function (req, res) {
                                 break;
                             case "hangout":
                                 obj_item.href = "/hangout/" + file.replace("." + file.split('.').pop(), '');
+                                obj_item.youtube = json.youtube;
+                                break;
+                            case "news":
+                                obj_item.href = "/noticia/" + file.replace("." + file.split('.').pop(), '');
                                 break;
                         }
 
@@ -129,21 +112,208 @@ exports.hook  = function (req, res) {
 
                         // create a json objet
                         var export_json = {};
-                        export_json.title = config.web.title;
-                        export_json.type = 'home';
-                        export_json.articles = new_item;
+
+                        //export_json.type = 'home';
+                        export_json.rows = new_item;
+                        switch(type){
+                            case "articles":
+                                export_json.title = 'Artículos - ' +  config.web.title;
+                                break;
+                            case "hangouts":
+                                export_json.title = 'Hangouts - ' +  config.web.title;
+                                break;
+                            case "news":
+                                export_json.title = 'Noticias - ' +  config.web.title;
+                                break;
+                            case "events":
+                                export_json.title = 'Eventos - ' +  config.web.title;
+                                break;
+                        }
+
 
                         // export a json file
-                        fs.writeFile("./articles.json", JSON.stringify(export_json), function (error) {
+                        fs.writeFile('./' + type + '.json', JSON.stringify(export_json), function (error) {
                             if (error) {
                                 res.send(error)
                             }
-                            res.redirect('/');
                         });
                     }
-
                 });
             });
+        });
+    }
+
+    function out() {
+        exec(config.repository.exec, puts);
+
+        create('articles');
+        create('hangouts');
+        create('news');
+
+        res.send('true');
+    }
+
+    return out();
+}
+
+// articles
+exports.articles = function (req, res) {
+    function out() {
+        var path_articles = path.join(__dirname, '../', 'articles.json');
+
+        fs.readFile(path_articles, 'utf-8', function (err, data) {
+            if (err){
+                res.send('Error loading JSON articles file');
+            }
+            else{
+                res.render('articles', { data: JSON.parse(data) });
+            }
+        });
+    }
+
+    return out();
+}
+
+// article
+exports.article = function (req, res) {
+    function out() {
+
+        // set path to article files
+        var path_article_json = path.join(__dirname, '../' + config.content.repository.name + '/articles', req.params.slug + '.json');
+        var path_article_markdown = path.join(__dirname, '../' + config.content.repository.name + '/articles', req.params.slug + '.markdown');
+
+        // load article json
+        fs.readFile(path_article_json, 'utf8', function (err1, article_json) {
+            if (err1) {
+                res.send('Error loading JSON article file');
+            }
+            else {
+                // load article markdown
+                fs.readFile(path_article_markdown, 'utf8', function (err2, article_markdown) {
+                    if (err2) {
+                        res.send('Error loading MARKDOWN article file');
+                    }
+                    else {
+                        // set article object
+                        var article_obj = JSON.parse(article_json);
+
+                        // load author json
+                        var path_author_json = path.join(__dirname, '../' + config.content.repository.name + '/authors', article_obj.author + '.json');
+
+                        fs.readFile(path_author_json, 'utf8', function (err3, author_json) {
+                            if (err3) {
+                                res.send('Error loading MARKDOWN article file');
+                            }
+                            else {
+                                // set markdown to html content in article object
+                                article_obj.content = md(article_markdown);
+
+                                // set date from string reading
+                                article_obj.date = moment(article_obj.date).fromNow();
+
+                                if (article_obj.update != "") {
+                                    article_obj.update = moment(article_obj.update).fromNow();
+                                }
+
+                                // set author object
+                                var author_obj = JSON.parse(author_json);
+
+                                // set md5 hash to load picture from gravatar.com
+                                author_obj.picture = crypto.createHash('md5').update(author_obj.email).digest("hex");
+
+                                // set author object on article object
+                                article_obj.author = author_obj;
+
+                                // load article template with article object
+                                res.render('article', { data: article_obj });
+                            }
+                        });
+
+                    }
+                });
+            }
+        });
+    }
+
+    return out();
+}
+
+// hangouts
+exports.hangout = function (req, res) {
+    function out() {
+        // set path to article files
+        var path_hangout_json = path.join(__dirname, '../' + config.content.repository.name + '/hangouts', req.params.slug + '.json');
+        var path_hangout_markdown = path.join(__dirname, '../' + config.content.repository.name + '/hangouts', req.params.slug + '.markdown');
+
+        // read file content
+        fs.readFile(path_hangout_json, 'utf-8', function (err1, hangout_json) {
+            if (err1) {
+                res.send('Error loading JSON hangout file');
+            }
+            else{
+                // load article markdown
+                fs.readFile(path_hangout_markdown, 'utf8', function (err2, hangout_markdown) {
+                    if (err2) {
+                        res.send('Error loading MARKDOWN hangout file');
+                    }
+                    else{
+                        // set article object
+                        var hangout_obj = JSON.parse(hangout_json);
+
+                        // load author json
+                        var path_author_json = path.join(__dirname, '../' + config.content.repository.name + '/authors', hangout_obj.author + '.json');
+
+                        fs.readFile(path_author_json, 'utf8', function (err3, author_json) {
+                            if (err3) {
+                                res.send('Error loading JSON author file');
+                            }
+                            else {
+                                hangout_obj.href = '/hangout/' + req.params.slug;
+
+                                // set markdown to html content in article object
+                                hangout_obj.content = md(hangout_markdown);
+
+                                // set date from string reading
+                                hangout_obj.date = moment(hangout_obj.date).fromNow();
+
+                                if (hangout_obj.update != "") {
+                                    hangout_obj.update = moment(hangout_obj.update).fromNow();
+                                }
+
+                                // set author object
+                                var author_obj = JSON.parse(author_json);
+
+                                // set md5 hash to load picture from gravatar.com
+                                author_obj.picture = crypto.createHash('md5').update(author_obj.email).digest("hex");
+
+                                // set author object on hangout object
+                                hangout_obj.author = author_obj;
+
+                                // load article template with hangout object
+                                res.render('hangout', { data: hangout_obj });
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    return out();
+}
+
+// hangouts
+exports.hangouts = function (req, res) {
+    function out() {
+        var path_hangouts = path.join(__dirname, '../', 'hangouts.json');
+
+        fs.readFile(path_hangouts, 'utf-8', function (err, data) {
+            if (err){
+                res.send('Error loading JSON hangouts file');
+            }
+            else{
+                res.render('hangouts', { data: JSON.parse(data) });
+            }
 
         });
     }
@@ -151,22 +321,82 @@ exports.hook  = function (req, res) {
     return out();
 }
 
-exports.hangouts = function (req, res) {
+// new
+exports.new = function (req, res) {
     function out() {
-        res.send('Hello world');
+
+        // set path to new files
+        var path_new_json = path.join(__dirname, '../' + config.content.repository.name + '/news', req.params.slug + '.json');
+        var path_new_markdown = path.join(__dirname, '../' + config.content.repository.name + '/news', req.params.slug + '.markdown');
+
+        // load new json
+        fs.readFile(path_new_json, 'utf8', function (err1, new_json) {
+            if (err1) {
+                res.send('Error loading JSON new file');
+            }
+            else {
+                // load new markdown
+                fs.readFile(path_new_markdown, 'utf8', function (err2, new_markdown) {
+                    if (err2) {
+                        res.send('Error loading MARKDOWN new file');
+                    }
+                    else {
+                        // set new object
+                        var new_obj = JSON.parse(new_json);
+
+                        // load author json
+                        var path_author_json = path.join(__dirname, '../' + config.content.repository.name + '/authors', new_obj.author + '.json');
+
+                        fs.readFile(path_author_json, 'utf8', function (err3, author_json) {
+                            if (err3) {
+                                res.send('Error loading JSON new file');
+                            }
+                            else {
+                                // set markdown to html content in new object
+                                new_obj.content = md(new_markdown);
+
+                                // set date from string reading
+                                new_obj.date = moment(new_obj.date).fromNow();
+
+                                if (new_obj.update != "") {
+                                    new_obj.update = moment(new_obj.update).fromNow();
+                                }
+
+                                // set author object
+                                var author_obj = JSON.parse(author_json);
+
+                                // set md5 hash to load picture from gravatar.com
+                                author_obj.picture = crypto.createHash('md5').update(author_obj.email).digest("hex");
+
+                                // set author object on new object
+                                new_obj.author = author_obj;
+
+                                // load article template with new object
+                                res.render('new', { data: new_obj });
+                            }
+                        });
+
+                    }
+                });
+            }
+        });
     }
 
     return out();
 }
 
-
-exports.home = function (req, res) {
+// news
+exports.news = function (req, res) {
     function out() {
-        var path_articles = path.join(__dirname, '../', 'articles.json');
+        var path_news = path.join(__dirname, '../', 'news.json');
 
-        fs.readFile(path_articles, 'utf-8', function (err, data) {
-            if (err) throw err;
-            res.render('index', { data: JSON.parse(data) });
+        fs.readFile(path_news, 'utf-8', function (err, data) {
+            if (err){
+                res.send('Error loading JSON news file');
+            }
+            else{
+                res.render('news', { data: JSON.parse(data) });
+            }
         });
     }
 
